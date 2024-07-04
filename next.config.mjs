df178d22200333db7b40b1d8bdfdc6b9f9e56a92
bernardomw1 <bernardomw@usp.br>
2024-07-04 20:03:57 -0300

    second

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
+	output: "export",  // <=== enables static exports
+	experimental: {
+		missingSuspenseWithCSRBailout: false,
+	},
 
 };
 
