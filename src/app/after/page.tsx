5a8198ec7359974c555c34707e6e0972f2dcf45b
bernardomw1 <bernardomw@usp.br>
2024-07-04 20:20:14 -0300

    third

--- src/app/after/page.tsx
+++ src/app/after/page.tsx
@@ -1,83 +1,111 @@
-'use client'
-import { Card, CardContent, Typography } from "@mui/material";
-import Image from "next/image";
-import React from "react";
-import adv from "../../images/adv.jpeg";
-import beer from "../../images/beer.jpeg";
-import home from "../../images/home.png";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+'use client';
+import { Card, CardContent, Typography } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+import adv from '../../images/adv.jpeg';
+import beer from '../../images/beer.jpeg';
+import home from '../../images/home.png';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 const AfterDinnerPage = () => {
-  const searchParams = useSearchParams()
- 
-  const param = searchParams.get('restaurant')
+  const searchParams = useSearchParams();
+
+  const param = searchParams.get('restaurant');
   return (
     <div className="backgroundImage h-screen flex items-center justify-center">
       <div className="w-[1000px] bg-[#fdf5ef] rounded-3xl flex">
         <Card className="w-1/3 mx-10">
-          <Link href={{pathname: 'endPage',query: {
-            restaurant: param,
-            after: 'adv'
-          }}}>
-          <Image src={adv} alt="" className="w-[420px] h-[200px]" />
+          <Link
+            href={{
+              pathname: 'endPage',
+              query: {
+                restaurant: param,
+                after: 'adv',
+              },
+            }}
+          >
+            <Image src={adv} alt="" className="w-[420px] h-[200px]" />
           </Link>
-          <Link href={{pathname: 'endPage',query: {
-            restaurant: param,
-            after: 'adv'
-          }}}>
-
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Adventure Motel{" "}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Muitas montanhas, natureza, piscinas naturais e exercícios físicos
-              hihihi
-            </Typography>
-          </CardContent>
-          </Link>
-        </Card>
-        <Card className="w-1/3 mx-10">
-          <Link href={{pathname: '',query: {
-            restaurant: param,
-            after: 'beer'
-          }}}>
-          <Image src={beer} alt="" className="w-[420px] h-[200px]" />
-          </Link>
-          <Link href={{pathname: '',query: {
-            restaurant: param,
-            after: 'beer'
-          }}}>
-
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Algum barzinho tomar uma cervejinha trincando{" "}
-            </Typography>
-            <Typography variant="body2" color="text.secondary"></Typography>
-          </CardContent>
+          <Link
+            href={{
+              pathname: 'endPage',
+              query: {
+                restaurant: param,
+                after: 'adv',
+              },
+            }}
+          >
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Adventure Motel{' '}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Muitas montanhas, natureza, piscinas naturais e exercícios
+                físicos hihihi
+              </Typography>
+            </CardContent>
           </Link>
         </Card>
         <Card className="w-1/3 mx-10">
-          <Link href={{pathname: '',query: {
-            restaurant: param,
-            after: 'home'
-          }}}>
-          <Image src={home} alt="" className="w-[333px] h-[200px]" />
+          <Link
+            href={{
+              pathname: 'endPage',
+              query: {
+                restaurant: param,
+                after: 'beer',
+              },
+            }}
+          >
+            <Image src={beer} alt="" className="w-[420px] h-[200px]" />
           </Link>
-          <Link href={{pathname: '',query: {
-            restaurant: param,
-            after: 'home'
-          }}}>
-
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Voltar pra casa{" "}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Ver um filminho, ficar bem enroladinha em mim e se amar muito bem coladinhos
-            </Typography>
-          </CardContent>
+          <Link
+            href={{
+              pathname: 'endPage',
+              query: {
+                restaurant: param,
+                after: 'beer',
+              },
+            }}
+          >
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Algum barzinho tomar uma cervejinha trincando{' '}
+              </Typography>
+              <Typography variant="body2" color="text.secondary"></Typography>
+            </CardContent>
+          </Link>
+        </Card>
+        <Card className="w-1/3 mx-10">
+          <Link
+            href={{
+              pathname: 'endPage',
+              query: {
+                restaurant: param,
+                after: 'home',
+              },
+            }}
+          >
+            <Image src={home} alt="" className="w-[333px] h-[200px]" />
+          </Link>
+          <Link
+            href={{
+              pathname: 'endPage',
+              query: {
+                restaurant: param,
+                after: 'home',
+              },
+            }}
+          >
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Voltar pra casa{' '}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Ver um filminho, ficar bem enroladinha em mim e se amar muito
+                bem coladinhos
+              </Typography>
+            </CardContent>
           </Link>
         </Card>
       </div>
--- src/app/endPage/page.tsx
+++ src/app/endPage/page.tsx
@@ -1,22 +1,22 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { Typography } from "@mui/material";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { Typography } from '@mui/material';
 
 enum Restaurants {
-  pop = "Pop Vegan",
-  mais = "Mais Burguinho",
-  mexi = "Mexicanissimo",
-  coruja = "A coruja",
-  prime = "Prime Dog",
-  bawa = "Bawarchi",
-  sao = "São Saruê",
+  pop = 'Pop Vegan',
+  mais = 'Mais Burguinho',
+  mexi = 'Mexicanissimo',
+  coruja = 'A coruja',
+  prime = 'Prime Dog',
+  bawa = 'Bawarchi',
+  sao = 'São Saruê',
 }
 
 enum Afters {
-  adv = " no Adventure Motel",
-  beer = "em algum barzinho",
-  home = "em casa",
+  adv = ' no Adventure Motel',
+  beer = ' em algum barzinho',
+  home = ' em casa',
 }
 
 interface Timer {
@@ -51,9 +51,9 @@ const EndPage = () => {
     let minutes: string | number = Math.floor((counter / (1000 * 60)) % 60);
     let hours: string | number = Math.floor((counter / (1000 * 60 * 60)) % 24);
 
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
     return {
       days: days,
@@ -63,9 +63,9 @@ const EndPage = () => {
     };
   }
   const searchParams = useSearchParams();
-  const after = searchParams.get("after");
+  const after = searchParams.get('after');
 
-  const restaurant = searchParams.get("restaurant");
+  const restaurant = searchParams.get('restaurant');
   return (
     <div className="backgroundImage h-screen flex items-center justify-center">
       <div className="w-[500px] bg-[#fdf5ef] rounded-3xl flex">
@@ -78,17 +78,18 @@ const EndPage = () => {
           rosto, seu corpo, seu cheiro.
           <br />
           <br />
-          Queria que você soubesse que faz{" "}
+          Queria que você soubesse que faz{' '}
           <span className="text-red-500">
             {counter.days} dias {counter.mins} minutos {counter.secs} segundos
-          </span>{" "}
+          </span>{' '}
           que sou a pessoa mais feliz desse mundo e sim, todo esse tempo foi
-          namorando você ❤️ Quero agradecer por cada segundo do seu lado porque assim eu consigo expressar um pouquinho do que eu sinto, você é
-          a coisinha mais especial do mundo todo. Amo você demais!
+          namorando você ❤️ Quero agradecer por cada segundo do seu lado porque
+          assim eu consigo expressar um pouquinho do que eu sinto, você é a
+          coisinha mais especial do mundo todo. Amo você demais!
           <br />
           <br />E por último, mas não menos importante, gostaria de deixar
-          registrado que teremos um jantar romântica no restaurante{" "}
-          <span className="text-red-500">{Restaurants[restaurant]}</span>{" "}e
+          registrado que teremos um jantar romântica no restaurante{' '}
+          <span className="text-red-500">{Restaurants[restaurant]}</span> e
           depois um after maravilhoso
           <span className="text-red-500">{Afters[after]}</span>
           <br />
